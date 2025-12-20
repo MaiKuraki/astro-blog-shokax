4a16663a68e8e41be480c8417a33ae420d4debf7
zkz098 <zkz20081204@126.com>
2025-12-20 10:52:53 +0800

    feat: 优化首屏加载

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import svelte from '@astrojs/svelte'
 
 // @ts-check
 import { defineConfig } from 'astro/config'
+import esToolkitPlugin from 'vite-plugin-es-toolkit';
 
 import UnoCSS from 'unocss/astro'
 
@@ -17,5 +18,8 @@ export default defineConfig({
         '@': new URL('./src', import.meta.url),
       },
     },
+    plugins: [
+      esToolkitPlugin()
+    ]
   },
 })
